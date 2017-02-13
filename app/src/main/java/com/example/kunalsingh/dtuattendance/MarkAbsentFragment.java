171797cc467dc75c095e9883b49a4833afbd961d
kinglesnar <singh.kunal112@gmail.com>
2017-02-13 13:38:25 +0530

    Teacher side present absent view Pager made

---
+++ app/src/main/java/com/example/kunalsingh/dtuattendance/MarkAbsentFragment.java
@@ -0,0 +1,38 @@
+package com.example.kunalsingh.dtuattendance;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kunalsingh on 13/02/17.
+ */
+
+public class MarkAbsentFragment extends Fragment {
+
+    ListView listView;
+    public MarkAbsentFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.mark_absent_frag,container,false);
+
+        ArrayList<String> studentsList = MarkingAttendaceTeacher.getStudents();
+        ArrayList<Integer> colorsList = MarkingAttendaceTeacher.getColors();
+        listView = (ListView)view.findViewById(R.id.student_list_view_abs);
+        ListViewAdapter listViewAdapter = new ListViewAdapter(studentsList,'a',getContext(),colorsList);
+        listView.setAdapter(listViewAdapter);
+        listViewAdapter.notifyDataSetChanged();
+
+        return view;
+    }
+}
